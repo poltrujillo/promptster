b26b22e92106fadf8b4191f9abd7def689094afc
Pol Trujillo <103571473+poltrujillo@users.noreply.github.com>
2024-06-05 17:38:04 +0200

    refactor: Update Background component to use CSS gradients for improved performance

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,10 +1,12 @@
 export default function Background() {
   return (
     <div className="fixed left-0 top-0 -z-10 h-full w-full">
-      <div className="absolute w-full h-full bg-[#0b0d13]">
-        <div className="absolute bottom-0 right-[-10%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(0,200,255,0.15),rgba(255,255,255,0))]"></div>
-        <div className="absolute bottom-0 left-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(0,255,242,0.15),rgba(255,255,255,0))]"></div>
-      </div>
+      <div
+        className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950"
+        style={{
+          backgroundImage: `linear-gradient(38.73deg, rgba(204, 0, 187, 0.25) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.25) 100%)`,
+        }}
+      ></div>
     </div>
   );
 }
